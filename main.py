e53c9cb0587f0ec83221cae2bdcd576eb6971ec2
filmon arefayne <filmon67@hotmail.it>
2020-01-09 11:59:54 +0100

    also for kernel = 2

--- main.py
+++ main.py
@@ -18,7 +18,6 @@ if __name__ == "__main__":
     error_last = []
     error_avg = []
     error_vote = []
-    # change the kernel to from 1 to 2
     kernel = 1
 
     print("epoch: from 0.1 to 0.9 kernel:{}".format(kernel))
@@ -31,6 +30,32 @@ if __name__ == "__main__":
         error_avg.append(e_a)
         error_vote.append(e_v)
     print("epoch: from 1 to 10 kernel:{}".format(kernel))
+    for i in tqdm(x2):
+        e_r, e_l, e_a, e_v = load_and_test(X_train, X_test, y_test, i, kernel)
+        error_random.append(e_r)
+        error_last.append(e_l)
+        error_avg.append(e_a)
+        error_vote.append(e_v)
+    log_plot(np.concatenate((x1, x2)), error_random, error_last, error_avg, error_vote, kernel) 
+
+    # now for kernel = 2
+
+    error_random = []
+    error_last = []
+    error_avg = []
+    error_vote = []
+    kernel = 2
+
+    print("epoch: from 0.1 to 0.9 kernel:{}".format(kernel))
+    x1 = np.arange(0.1, 1, 0.1)
+    x2 = np.arange(1, 31)
+    for i in tqdm(x1):
+        e_r, e_l, e_a, e_v = load_and_test(X_train, X_test, y_test, i, kernel)
+        error_random.append(e_r)
+        error_last.append(e_l)
+        error_avg.append(e_a)
+        error_vote.append(e_v)
+    print("epoch: from 1 to 30 kernel:{}".format(kernel))
     for i in tqdm(x2):
         e_r, e_l, e_a, e_v = load_and_test(X_train, X_test, y_test, i, kernel)
         error_random.append(e_r)
